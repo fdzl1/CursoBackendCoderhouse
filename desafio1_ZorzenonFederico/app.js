1c8abd19b51b99231c33691968cddee305a81ef1
Fede Z <fedenzlutn@gmail.com>
2022-08-16 17:56:51 -0300

    Agregando correcciones al desafio 1-Backend/Coderhouse

--- desafio1_ZorzenonFederico/app.js
+++ desafio1_ZorzenonFederico/app.js
@@ -1,8 +1,6 @@
 import { usuarios } from './array.js';
 
 
-
-
 class Usuario{
     constructor(nombre, apellido, libros, mascotas) {
         this.nombre = nombre;
@@ -33,17 +31,19 @@ class Usuario{
 }
 
 
-
+// Creo un array de objetos Usuario
 const usuariosArray = usuarios.map(u => new Usuario(u.nombre, u.apellido, u.libros, u.mascotas));
+// Pruebo los metodos de la clase usuario
 console.log(usuariosArray[0].getFullName());
 console.log(usuariosArray[0].countMascotas());
+// Agrego una mascota y muestro el nombre de la misma
 usuariosArray[0].addMascota('Oso');
 console.log(usuariosArray[0].mascotas[usuariosArray[0].countMascotas()-1]);
 
+// Agrego un libro y lo muestro
 usuariosArray[0].addBook('Libro2', 'autor2');
-
 console.log(usuariosArray[0].libros[1]);
-
+// Genero el listado de nombre de libros de un usuario
 console.log(usuariosArray[0].getBookNames())
 
 
